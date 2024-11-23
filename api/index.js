2df67beabea30b3585f4312ac167a93b140f90ca
M K Saifullah Mollick <memks.mollick@gmail.com>
2024-11-23 17:17:38 +0600

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import router from "./routes/user.route.js";
+("./routes/user.route.js");
 dotenv.config();
 
 mongoose
@@ -10,3 +12,5 @@ mongoose
 const app = express();
 
 app.listen(3000, console.log("Server is running..."));
+
+app.use("/api/user", router);
