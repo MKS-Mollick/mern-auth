5dd3f471f5b072ec2cd159dac49c7fbc1e512631
M K Saifullah Mollick <memks.mollick@gmail.com>
2024-11-23 13:38:18 +0600

    Header section to component folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/about" element={<About />}></Route>
         <Route path="/" element={<Home />} />
