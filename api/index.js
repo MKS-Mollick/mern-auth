ef82d047e8468dcfd1cc1bd6c04da8e8b44066e1
M K Saifullah Mollick <memks.mollick@gmail.com>
2024-11-23 15:12:05 +0600

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connection successfull"))
+  .catch((err) => console.log(err));
 const app = express();
 
 app.listen(3000, console.log("Server is running..."));
